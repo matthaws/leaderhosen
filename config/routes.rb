b3a42e3dafb3c72ebc37ed215b60a3638edfaa09
Matt Haws <matthaws@gmail.com>
2017-09-30 14:52:25 -0400

    Add auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
+
+  scope :auth do
+    get 'is_signed_in', to: 'auth#is_signed_in?'
+  end
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
