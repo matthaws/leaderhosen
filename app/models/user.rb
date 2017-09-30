d28af7188c480f1c050d3c99ec6dc114edcc244c
Matt Haws <matthaws@gmail.com>
2017-09-30 15:11:57 -0400

    Add Facebook Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :omniauthable => [:facebook]
+         :confirmable
+
+  devise :omniauthable, omni_providers: [:facebook]
+
+  def self.from_omniauth(auth)
+    User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.image = auth.info.image # assuming the user model has an image
+  
+      user.skip_confirmation!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   scope :auth do
     get 'is_signed_in', to: 'auth#is_signed_in?'
   end
-  
-  devise_for :users
+
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
