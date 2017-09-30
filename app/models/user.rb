0a5bed604eb775d9d7d3c9901596cac0bf4dd00d
Matt Haws <matthaws@gmail.com>
2017-09-30 15:16:56 -0400

    Add User model omniauth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ApplicationRecord
     User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.image = auth.info.image # assuming the user model has an image
-  
+
       user.skip_confirmation!
     end
   end
