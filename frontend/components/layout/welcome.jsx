85de336907dea186695a8b4f58eda588c4251c86
Matt Haws <matthaws@gmail.com>
2017-10-22 15:08:34 -0400

    Add scrolling effect and background image to welcome screen

--- frontend/components/layout/welcome.jsx
+++ frontend/components/layout/welcome.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Welcome = () => {
   return (
-    <div className='welcomeBox'>
-      <h1>Welcome to Leaderhosen</h1>
-      <p>Keep score with your friends</p>
-    </div>
+    <main className='welcomeContent'>
+      <div className='welcomeBox'>
+        <div className='welcomeTextBox'>
+          <h1>Welcome to Leaderhosen</h1>
+          <p>Keep score with your friends</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
